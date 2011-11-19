e05fa5fa643604ae09a64c40c22795e8348b0225
Daniel Farina <drfarina@acm.org>
2011-11-19 13:00:20 -0800

    Tersify tests
    Signed-off-by: Daniel Farina <drfarina@acm.org>

--- test/test_redrat.rb
+++ test/test_redrat.rb
@@ -3,27 +3,27 @@ require "redrat"
 
 class TestRedrat < Test::Unit::TestCase
   def test_module_declaration
-    Object::RedRat
+    RedRat
   end
 
   def test_builtin_existence
-    Object::RedRat::builtins
+    RedRat::builtins
   end
 
   def test_property_access
-    Object::RedRat::builtins['str'].join
+    RedRat::builtins['str'].join
   end
 
   def test_must_reject_no_args_to_apply
     begin
-      Object::RedRat::apply
+      RedRat::apply
     rescue ArgumentError
     end
   end
 
   def test_must_reject_blocks
     begin
-      Object::RedRat::apply 'hello' do |blah|
+      RedRat::apply 'hello' do |blah|
       end
     rescue ArgumentError
     end
@@ -31,20 +31,20 @@ class TestRedrat < Test::Unit::TestCase
 
   def test_must_reject_non_python_values_in_first_argument
     begin
-      Object::RedRat::apply 'hello' '1', '2'
+      RedRat::apply 'hello' '1', '2'
     rescue ArgumentError
     end
   end
 
   def test_function_call_from_string
-    int_parse_function = Object::RedRat::builtins['int']
-    Object::RedRat::apply(int_parse_function, '42')
+    int_parse_function = RedRat::builtins['int']
+    RedRat::apply(int_parse_function, '42')
   end
 
   def test_truth
-    int_parse_function = Object::RedRat::builtins['int']
-    pv_42 = Object::RedRat::apply(int_parse_function, '42')
-    pv_32 = Object::RedRat::apply(int_parse_function, '32')
+    int_parse_function = RedRat::builtins['int']
+    pv_42 = RedRat::apply(int_parse_function, '42')
+    pv_32 = RedRat::apply(int_parse_function, '32')
 
     if pv_42 < pv_32
       raise
